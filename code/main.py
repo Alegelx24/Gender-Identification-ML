860c03efb7ad2c9c1304ad9ffaa4b4e4b3a1e690
Alessandro Gelsi <79928745+Alegelx24@users.noreply.github.com>
2023-08-25 16:43:22 +0200

    gaussian working with znorm feature

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ k=5 #K-fold
   
 def main():
     D,L = util.load_training_set('./data/Train.txt')
-    #D= util.scale_ZNormalization(D)[0]
+    D_norm= util.scale_ZNormalization(D)[0]
     #D_gaussianized= util.gaussianize_training(D)
     
     gaussianize= False 
@@ -21,6 +21,8 @@ def main():
     print("VALIDATION WITHOUT GAUSSIANIZATION")
 
     train_evaluate_gaussian_models(D, L)
+    train_evaluate_gaussian_models_zscore(D_norm, L)
+
     
 
 
@@ -38,7 +40,7 @@ def train_evaluate_gaussian_models(D,L):
     Options={ }  
     D_copy = D
     m = 12
-    while m>=9:
+    while m>=7:
         if m < 12:
             D = util.pca(m, D)[0]
             print ("##########################################")
@@ -47,6 +49,37 @@ def train_evaluate_gaussian_models(D,L):
         else:
             print ("##########################################")
             print ("#### Gaussian classifiers with NO PCA ####")
+            print ("##########################################")
+                    
+        
+        for pi in [0.1, 0.5, 0.9]:
+            min_dcf_full = validate.kfold(D, L, k, pi, gaussian.compute_score_full, Options)[0]
+            print(" Full-Cov - pi = %f -> minDCF = %f" %(pi,min_dcf_full))
+            min_dcf_diag = validate.kfold(D, L, k, pi, gaussian.compute_score_diag, Options)[0]
+            print(" Diag-cov - pi = %f -> minDCF = %f" %(pi,min_dcf_diag))
+            min_dcf_tied_full = validate.kfold(D, L, k, pi, gaussian.compute_score_tied_full, Options)[0]
+            print(" Tied full-cov - pi = %f  minDCF = %f" %(pi,min_dcf_tied_full))
+            min_dcf_tied_diag = validate.kfold(D, L, k, pi, gaussian.compute_score_tied_diag, Options)[0]
+            print(" Tied diag-cov - pi = %f  minDCF = %f" %(pi,min_dcf_tied_diag))
+
+        m=m-1
+        D=D_copy #restore original dataset
+
+
+
+def train_evaluate_gaussian_models_zscore(D,L):
+    Options={ }  
+    D_copy = D
+    m = 12
+    while m>=7:
+        if m < 12:
+            D = util.pca(m, D)[0]
+            print ("##########################################")
+            print ("##### Gaussian classifiers with m = %d and Z-score norm ####" %m)
+            print ("##########################################")
+        else:
+            print ("##########################################")
+            print ("#### Gaussian classifiers with NO PCA and Z-score norm ####")
             print ("##########################################")
                     
         
