40d16290e9fb59152fd162201449e4394c280522
Alessandro Gelsi <79928745+Alegelx24@users.noreply.github.com>
2023-08-26 16:06:34 +0200

    fix

--- code/gaussian_classifier.py
+++ code/gaussian_classifier.py
@@ -8,22 +8,6 @@ def mcol(v):
 def mrow(v):
     return v.reshape((1,v.size))
 
-#not used for report
-def split_db_2to1(D, L, seed=0):
-    nTrain = int(D.shape[1]*2.0/3.0)
-    numpy.random.seed(seed)
-    idx = numpy.random.permutation(D.shape[1])
-    idxTrain = idx[0:nTrain]
-    idxTest = idx[nTrain:]
-    
-    DTR = D[:, idxTrain]
-    DTE = D[:, idxTest]
-    LTR = L[idxTrain]
-    LTE = L[idxTest]
-    
-    return (DTR, LTR), (DTE, LTE)
-
-
 def full_compute_mean_covariance(D):
     mu=mcol(D.mean(1))
     C=numpy.dot(D-mu, (D-mu).T)/float(D.shape[1])
